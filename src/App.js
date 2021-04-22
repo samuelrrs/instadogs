43180e2ab78f94c716263582638de3acab679875
Samuel Ribeiro <samuelrrsouza@gmail.com>
2021-04-22 17:13:03 -0300

    feat: Adição do projeto inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/" element={<Login/>}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Login/LoginCreate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginCreate = () => {
+    return (
+        <div>
+          <h1>adasdasdasdasd</h1>  
+        </div>
+    )
+}
+
+export default LoginCreate
