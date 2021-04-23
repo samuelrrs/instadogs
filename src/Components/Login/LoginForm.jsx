c7d3c848687751c7842eb9559fe06944510f4815
Samuel Ribeiro <samuelrrsouza@gmail.com>
2021-04-23 18:06:32 -0300

    feat: Adição do function de buscar usuário

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,34 +3,44 @@ import { Link } from "react-router-dom";
 import useForm from "../../Hooks/useForm";
 import Button from "../Forms/Button/Button";
 import Input from "../Forms/Input/Input";
+import { TOKEN_POST, USER_GET } from "../../Services/api";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  console.log(username);
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    fetch("https://dogsapi.origamid.dev/json/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({}),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-      });
+  const getUser = async (token) => {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
+    }
+  };
+
   return (
     <section>
-      Login Form
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -8,36 +8,37 @@ const types = {
 };
 
 const useForm = (type) => {
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(null);
-
-  function validate(value) {
-    if (type === false) return true;
-    if (value.length === 0) {
-      setError("Preencha um valor.");
-      return false;
-    } else if (types[type] && !types[type].regex.test(value)) {
-      setError(types[type].message);
-      return false;
-    } else {
-      setError(null);
-      return true;
+    const [value, setValue] = React.useState('');
+    const [error, setError] = React.useState(null);
+  
+    function validate(value) {
+      if (type === false) return true;
+      if (value.length === 0) {
+        setError('Preencha um valor.');
+        return false;
+      } else if (types[type] && !types[type].regex.test(value)) {
+        setError(types[type].message);
+        return false;
+      } else {
+        setError(null);
+        return true;
+      }
     }
-  }
-
-  function onChange({ target }) {
-    if (error) validate(target.value);
-    setValue(target.value);
-  }
-
-  return {
-    value,
-    setValue,
-    onChange,
-    error,
-    validate: () => validate(value),
-    onBlur: () => validate(value),
+  
+    function onChange({ target }) {
+      if (error) validate(target.value);
+      setValue(target.value);
+    }
+  
+    return {
+      value,
+      setValue,
+      onChange,
+      error,
+      validate: () => validate(value),
+      onBlur: () => validate(value),
+    };
   };
-};
+  
 
 export default useForm;
