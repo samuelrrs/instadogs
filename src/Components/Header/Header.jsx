dfed21a2ae0a73f73d08958fff9ef69d3d30d0c9
Samuel Ribeiro <samuelrrsouza@gmail.com>
2021-04-26 12:36:34 -0300

    feat: Adição dos components estilizados do footer e header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,6 +7,7 @@ import { UserContext } from "../../Contexts/UserContext";
 
 const Header = () => {
   const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
